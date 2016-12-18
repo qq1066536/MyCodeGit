5c708837dda927d4f3406c5626573e780893feab
qq1066536 <1066536184@qq.com>
2016-12-18 20:28:26 +0800

    Signed-off-by: qq1066536 <1066536184@qq.com>

--- shell/Insatll/auto_install_lanmp/auto_install.sh
+++ shell/Insatll/auto_install_lanmp/auto_install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 path=~/test/sh/
-#source ./auto_tar.sh
-#cd $path
-#source ./Install_nginx.sh
-#cd $path
-#source ./mysql.sh
-#cd $path
-#source ./apache.sh
-#cd $path
-#source ./php.sh
+source ./auto_tar.sh
+cd $path
+source ./Install_nginx.sh
+cd $path
+source ./mysql.sh
+cd $path
+source ./apache.sh
+cd $path
+source ./php.sh
 
 cd ~/test/tar_gz/
 source ./auto_clean.sh
