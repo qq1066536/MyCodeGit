ab9b581481546d05a8f7d2a056aad57ab7d49b7e
qq1066536 <1066536184@qq.com>
2016-12-24 11:01:05 +0800

    Signed-off-by: qq1066536 <1066536184@qq.com>

---
+++ python/tab.py
@@ -0,0 +1,20 @@
+import sys   
+import readline   
+import rlcompleter   
+import atexit   
+import os    
+# tab completion   
+readline.parse_and_bind('tab: complete')   
+# history file   
+System=os.name
+if System=='nt':
+    histfile = os.path.join(os.environ['HOMEPATH'], '.pythonhistory')
+elif System=='posix':   
+    histfile = os.path.join(os.environ['HOME'], '.pythonhistory')   
+try:   
+    readline.read_history_file(histfile)   
+except IOError:   
+    pass   
+atexit.register(readline.write_history_file, histfile)   
+           
+del os, histfile, readline, rlcompleter  
